15a40f04642d847d2fe5389008f50f5ed97af417
Ricardo olvera <ivanmartinezgudino55@gmail.com>
2024-09-24 11:17:45 -0600

    subi mis archivos

---
+++ materias.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+materiasCallback({
+    "materias": [
+        {
+            "nombre": "Programación",
+            "codigo": "IS101",
+            "profesor": "Dr. López",
+            "creditos": 8,
+            "aula": "A1"
+        },
+        {
+            "nombre": "Redes",
+            "codigo": "IS102",
+            "profesor": "Mtro. García",
+            "creditos": 6,
+            "aula": "A2"
+        },
+        {
+            "nombre": "Bases de Datos",
+            "codigo": "IS103",
+            "profesor": "Mtro. Pérez",
+            "creditos": 7,
+            "aula": "A3"
+        },
+        {
+            "nombre": "Seguridad Informática",
+            "codigo": "IS104",
+            "profesor": "Dra. Martínez",
+            "creditos": 8,
+            "aula": "A4"
+        }
+        // Puedes agregar más materias aquí
+    ]
+});
